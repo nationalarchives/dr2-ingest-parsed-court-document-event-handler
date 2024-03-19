09b8ec4809e7215062b6ad0661fbd008a896d4ea
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-03-19 08:22:29 +0000

    Update fs2-io to 3.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ object Dependencies {
   lazy val logbackVersion = "2.23.0"
   lazy val pureConfigVersion = "0.17.6"
   lazy val daAwsClientsVersion = "0.1.39"
-  private val fs2Version = "3.9.4"
+  private val fs2Version = "3.10.0"
   private val circeVersion = "0.14.6"
   private val log4CatsVersion = "2.6.0"
 
